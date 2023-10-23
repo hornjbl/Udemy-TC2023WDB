24a7907c3636b122a3cabeeac7d848e5d7824d6f
Ben-Lindsey Horn <55430893+hornjbl@users.noreply.github.com>
2023-10-23 18:54:14 +0200

    Updates to section 18

--- section-18/drum-kit/index.js
+++ section-18/drum-kit/index.js
@@ -19,7 +19,58 @@ for (let i = 0; i < allButtons.length; i++) {
 		
 		allButtons[i].addEventListener("click", function () {
 			const audioDrum = new Audio(`./sounds/drum-${this.classList[0]}.mp3`);
+			buttonAnimation(this.classList[0]);
 
 			audioDrum.play();
     });	
+}
+
+document.addEventListener("keydown", (event) => {
+	const keyPressed = event.key;
+	
+	switch (keyPressed) {
+		case "w":
+			const drumW = new Audio(`./sounds/drum-W.mp3`);
+			drumW.play();
+			// drumW.cloneNode().play();
+			break;
+		case "a":
+			const drumA = new Audio(`./sounds/drum-A.mp3`);
+			drumA.play();
+			break;
+		case "s":
+			const drumS = new Audio(`./sounds/drum-S.mp3`);
+			drumS.play();
+			break;
+		case "d":
+			const drumD = new Audio(`./sounds/drum-D.mp3`);
+			drumD.play();
+			break;
+		case "j":
+			const drumJ = new Audio(`./sounds/drum-J.mp3`);
+			drumJ.play();
+			break;
+		case "k":
+			const drumK = new Audio(`./sounds/drum-K.mp3`);
+			drumK.play();
+			break;
+		case "l":
+			const drumL = new Audio(`./sounds/drum-L.mp3`);
+			drumL.play();
+			break;
+		default:
+			break;
+	};
+	buttonAnimation(keyPressed);
+});
+
+function buttonAnimation(currentKey) {
+	const elButton = document.getElementsByClassName(`${currentKey}`)[0];
+
+	elButton.classList.add("pressed");
+
+	setTimeout(() => {
+		elButton.classList.remove("pressed");
+	},100);
+	
 }
